4a0b7e4a9ba59fac539ab5bbe2b76ec15474f1be
Kaspar Allenbach <bonjour@kasparallenbach.ch>
2018-07-25 11:39:44 +0200

    added static translation files

---
+++ translations/fr-CH/site.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Das ist' => 'C\'est',
+    'Aktuelle Seitensprache' => 'Langue courante',
+    'Aktuelles Sprachhandle' => 'Langue pognée',
+    'Aktueller Sprachname' => 'Langue Nom',
+    'Aktueller Sprachkurzname' => 'Langue Nom brève',
+    'Aktuelle SprachID' => 'Langue identité',
+    'Sprachwechsler' => 'Changement de Langue',
+    'Liste aller IDs' => 'La liste de Identité numerique :D',
+];
---
+++ translations/it-CH/site.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'Das ist' => 'È esattemente',
+    'Aktuelle Seitensprache' => 'È esattemente pagina Linguale',
+    'Aktuelles Sprachhandle' => 'È esattemente handle Linguale',
+    'Aktueller Sprachname' => 'È esattemente Nome Linguale',
+    'Aktueller Sprachkurzname' => 'È esattemente Language abbreviazione',
+    'Aktuelle SprachID' => 'È esattemente Linguale ID',
+    'Sprachwechsler' => 'Language cambiavalute',
+    'Liste aller IDs' => 'ID Lista',
+    'Navigation' => 'Navigazione'
+];
