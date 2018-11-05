8db30675023df0027f28f68afe6ba3300a5cfef0
Thomas Lahn <thomas.lahn@meteotest.ch>
2018-11-05 04:30:16 +0100

    Monitoring/Chart: Add basic C3 chart (GLAMOS-28)

--- theme_src/js/glamos.js
+++ theme_src/js/glamos.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import 'jquery.mmenu';
 import 'lightgallery';
+import c3 from 'c3';
 import ieDetector from './ieDetector';
 ieDetector();
 
@@ -38,6 +39,34 @@ $(document).ready(function () {
     }
     else {
       location.hash = HREF_VALUE;
+    }
+  });
+
+  const chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ],
+      axes: {
+        data2: 'y2'
+      }
+    },
+    axis: {
+      y: {
+        label: { // ADD
+          text: 'Y Label',
+          position: 'outer-middle'
+        }
+      },
+      y2: {
+        show: true,
+        label: { // ADD
+          text: 'Y2 Label',
+          position: 'outer-middle'
+        }
+      }
     }
   });
 });
