78e98338f7b33a5dbb8e70c16067ad678aaf1675
FV Florian Verdet <florian.verdet@meteotest.ch>
2018-11-28 09:04:01 +0100

    js/map enableSearch: fix use feature.get() instead .values_

--- theme_src/js/map/map.js
+++ theme_src/js/map/map.js
@@ -224,7 +224,7 @@ function enableSearch( gletscher_features) {
       if( ! searchInput.length || ! gletscher_features.length)  return;
       for( var i = 0; i < gletscher_features.length; i++) {
         var gl = gletscher_features[i];
-        searchData[i] = { label: gl.values_[DISPLAY_NAME], value: gl };
+        searchData[i] = { label: gl.get(DISPLAY_NAME), value: gl };
       }
 
       function onSelect(ev, ui) {
