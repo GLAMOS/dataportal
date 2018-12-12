bc1659e356b7049c5db2d93420a5e65a9d97a17a
FV Florian Verdet <florian.verdet@meteotest.ch>
2018-12-12 07:55:45 +0100

    init js/map/monitoringSidepane: module for sidepane (switching...) code

---
+++ theme_src/js/map/monitoringSidepane.js
@@ -0,0 +1,28 @@
+
+import controller from '../controller'
+import datastore from '../datastore'   // the one feature (glacier) which is selected
+
+
+// -----
+// constants
+
+// are directly the CSS classes of the corresponding box nodes
+const SIDE_GLACIERS = 'sidebarControls'
+const SIDE_LAYERS = 'layerswitcher'
+const SIDE_MEASURE = 'latestMeasurements'   // separated only for mobile
+
+
+// -----
+// variables
+
+
+// -----
+// helpers
+
+
+// -----
+
+
+// -----
+// exports
+
