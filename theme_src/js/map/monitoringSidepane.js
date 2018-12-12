4fc74c77c4a59a1be73dadfaa57ac2d84c64cc39
FV Florian Verdet <florian.verdet@meteotest.ch>
2018-12-12 10:19:37 +0100

    map/monitorSidepane: add render() implementing toggle&content switching +
    (depends .hidden in default.scss from other not-yet-merged branch)

--- theme_src/js/map/monitoringSidepane.js
+++ theme_src/js/map/monitoringSidepane.js
@@ -34,11 +34,31 @@ function setup() {
 
 
 // -----
+// Rendering
+
+function render() {
+  const tabName = datastore.sidepane
+  const pane = $('.sidebarContainer')
+  const CLS_ACTIVE = 'active'
+  const CLS_HIDE = 'hidden'
+
+  // reset to unhighlight toggle and hide all content
+  $(TOGGLES).removeClass(CLS_ACTIVE)
+  $('> *', pane).addClass(CLS_HIDE)
+
+  // show/highlight what needs to be
+  $(`${TOGGLES}[data-tab="${tabName}"]`).addClass(CLS_ACTIVE)
+  $(`.${tabName}`, pane).removeClass(CLS_HIDE)
+}
+
+
+// -----
 // Tab switching
 
 function goToSidebarTab( tabName=SIDE_GLACIERS) {
   // TODO: if mobile, leave sidepane closed (=null(?))
   datastore.sidepane = tabName
+  render()
 }
 controller.bridge({goToSidebarTab})
 
