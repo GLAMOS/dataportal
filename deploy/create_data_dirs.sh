88bcdec836171530876a436ae5f01cdf8f9a015e
FV Florian Verdet <florian.verdet@meteotest.ch>
2018-12-21 08:30:45 +0100

    init deploy/create_data_dirs.sh: folder structure for VAW uploads &co +
    Documented on glamos-wiki:
    https://wiki.glamos.ch/doku.php?id=website:schnittstellen

---
+++ deploy/create_data_dirs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# top-level space, separate from GIT-Repo or DocRoot
+mkdir data
+
+
+# where data typically comes from
+mkdir data/from_vaw
+mkdir data/cms_uploads
+
+
+# list of all glaciers, currently for single JSON file
+mkdir data/from_vaw/inventory
+
+# glacier photos/pictures for the factsheet
+mkdir data/from_vaw/glacier_images
+
+# glacier descriptions for the factsheet
+mkdir data/from_vaw/glacier_infos
+
+# files that will be available in the Download section
+mkdir data/cms_uploads/downloads
+
+
+# varia fully managed by VAW, indipendent of Website, shall be web-available
+mkdir data/vaw
+
