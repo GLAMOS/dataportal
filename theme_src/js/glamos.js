GLAMOS/dataportal

commit ace366a7b43057f0fb506871474292ed1ce6759b

move glacier chart management into its own component

theme_src/js/glamos.js
@@ -9,7 +9,6 @@ import controller from './controller';
 
 import './map/map.js';
 import sidepane from './map/monitoringSidepane';
-import { Graph, configs, Queue } from './chart';
 
 
 (function (global, $) {
@@ -27,64 +26,11 @@ import { Graph, configs, Queue } from './chart';
     $(`#${TAB_ID}`).addClass(CLASS_NAME);
   }
   controller.bridge({selectDownloadTab});
+});
 
-  let select_type;
-  
-  const graph = Graph('#chart');
-  let queue;
 
-  controller.bridge({
-    /**
-     * Load data for glaciers
-     *
-     * @param  {Array[string]} ids  Glacier IDs
-     * @param  {Object} options
-     *   | Property | Meaning |
-     *   |:-------- |:--------|
-     *   | clear    | If <code>true</code>, previous data will be unloaded. Default: <code>false</code>. |
-     */
-    loadGlacierData (glacierIds, options = {clear: false}) {
-      // HACK do nothing if we're called for the wrong page
-      if (!select_type.length) return;
-
-      // Flag clear is set when the chart type changes.
-      // In this case we drop all pending queries and
-      // start a new queue with the new config
-      if (options.clear) {
-        if (queue) {
-          queue.cancel();
-          queue = false; // Will be created with new config
-        }
-        graph.clear();
-      }
-
-      const type = select_type.val();
-      const config = configs[type];
-
-      if (!queue) {
-        queue = Queue(config, (data) => graph.show(config, data));
-      }
-
-      for (let id of glacierIds) queue.load(id);
-    },
-
-    /**
-     * Unload the data of a specific glacier
-     * @param  {string} id  Glacier ID
-     */
-    unloadGlacierData (id) {
-      graph.unload(id);
-    }
-  });
-
-  $(document).ready(() => {
-    /* initializing */
-    controller.onPageLoad();
-    sidepane.setup();
-
-    select_type = $('#chart_param');
-    select_type.change(function() {
-      controller.switchChartType(select_type.val());
-    });
-  });
-}(global, $));
+$(document).ready(() => {
+  /* initializing */
+  controller.onPageLoad();
+  sidepane.setup();
+});

theme_src/js/controller.js
@@ -4,6 +4,7 @@ import urlManager from './UrlManager';
 import datastore from './datastore';
 
 import factsheetDetails from './factsheetDetails'
+import { Dataview } from './dataview';
 
 
 /* Constants */
@@ -25,6 +26,11 @@ function factsheetUpdate(feature) {
 
 
 /** Our Controller (Action → Reaction) */
+
+// HACK should be instance variable on Controller but Controller is
+// used with Proxy which expects that all instance variables are functions.
+const dataview = Dataview();
+
 class Controller {
   _bootstrapFromState () {
     if (datastore.downloadTab) {
@@ -34,7 +40,7 @@ class Controller {
     if (feature) {
       bridge.selectGlacier(feature);
       bridge.mapPanTo(feature);
-      bridge.loadGlacierData(datastore.selectedGlaciers.get());
+      dataview.load(datastore.selectedGlaciers.get());
       factsheetUpdate(feature)
     }
     bridge.monitoringSelectedFeatureList.refresh();
@@ -44,7 +50,7 @@ class Controller {
     const feature = datastore.features.findById(bridge.getRandomVIP());
     if (feature) {
       bridge.selectGlacier(feature);
-      bridge.loadGlacierData([feature2id(feature)], {clear: true});
+      dataview.load([feature2id(feature)], {clear: true});
       bridge.mapPanTo(feature);
       bridge.monitoringSelectedFeatureList.add(feature);
     }
@@ -80,6 +86,8 @@ class Controller {
 
   //onPageLoad(page) {
   onPageLoad () {
+    dataview.setup(controller);
+
     urlManager.loadState();
     this._setFallbackState();
     this._bootstrapFromState();
@@ -103,7 +111,7 @@ class Controller {
 
   mapMarkerHighlighted (feature) {
     bridge.selectGlacier(feature);
-    bridge.loadGlacierData([feature2id(feature)]);
+    dataview.load([feature2id(feature)]);
     /* note: no map panning */
     bridge.monitoringSelectedFeatureList.add(feature);
     urlManager.majorUpdate();
@@ -111,7 +119,7 @@ class Controller {
 
   searchSelected (feature) {
     bridge.selectGlacier(feature);
-    bridge.loadGlacierData([feature2id(feature)]);
+    dataview.load([feature2id(feature)]);
     bridge.mapPanTo(feature);
     bridge.monitoringSelectedFeatureList.add(feature);
     factsheetUpdate(feature)
@@ -129,7 +137,7 @@ class Controller {
 
   selectionListRemove (id) {
     datastore.selectedGlaciers.remove(id);
-    bridge.unloadGlacierData(id);
+    dataview.unload(id);
 
     /* Select last entry in selected glaciers list */
     // this.selectionListHighlight( datastore.selectedGlaciers.get().slice(-1)[0] )
@@ -143,7 +151,7 @@ class Controller {
   }
 
   switchChartType (type) {
-    bridge.loadGlacierData(datastore.selectedGlaciers.get(), {clear: true});
+    dataview.loadGlacierData(datastore.selectedGlaciers.get(), {clear: true});
 
     /* TODO: Update URL */
   }

commit f623623d078b1e591d72cddea545ce29d49473c0

js/glamos: fix loading download-tab code, bug introduced in ace366a

theme_src/js/glamos.js
@@ -26,7 +26,7 @@ import sidepane from './map/monitoringSidepane';
     $(`#${TAB_ID}`).addClass(CLASS_NAME);
   }
   controller.bridge({selectDownloadTab});
-});
+}(global, $));
 
 
 $(document).ready(() => {