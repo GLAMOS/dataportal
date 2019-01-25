7126b21a52e397d1e4bf8642e028212e231ec21f
FV Florian Verdet <florian.verdet@meteotest.ch>
2019-01-25 11:28:23 +0100

    js/map/layer/*_wmts: fix attribution swisstopo link, GLAMOS-138

--- theme_src/js/map/layer/eiszeit_wmts.js
+++ theme_src/js/map/layer/eiszeit_wmts.js
@@ -5,7 +5,7 @@ import XYZLayer from 'ol/source/XYZ';
 var eiszeit_wmts = new TileLayer({
     source: new XYZLayer({
       url: 'https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.geologie-eiszeit-lgm-raster/default/20081231/3857/{z}/{x}/{y}.png',
-      attributions: '© <a target="new" href="https://www.swisstopo.admin.ch/internet/swisstopo/en/home.html">swisstopo</a>'
+      attributions: '© <a target="new" href="https://www.swisstopo.admin.ch/">swisstopo</a>'
     }),
     baseLayer: true,
     title: "Eiszeit max. Ausdehnung"
--- theme_src/js/map/layer/pixelkarte_farbe_wmts.js
+++ theme_src/js/map/layer/pixelkarte_farbe_wmts.js
@@ -4,7 +4,7 @@ import XYZLayer from 'ol/source/XYZ';
 var pixelkarte_farbe_wmts = new TileLayer({
     source: new XYZLayer({
       url: 'https://wmts10.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg',
-      attributions: '© <a target="new" href="https://www.swisstopo.admin.ch/internet/swisstopo/en/home.html">swisstopo</a>'
+      attributions: '© <a target="new" href="https://www.swisstopo.admin.ch/">swisstopo</a>'
     }),
     baseLayer: true,
     title: "Landeskarte farbig"
